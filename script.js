8c93df7a85ca32c625d42cfada4a78fcdcf29168
Ufhano <4035709@myuwc.ac.za>
2025-10-02 23:02:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log('Hello, World!');
+
+document.querySelector('.btn-open');
+
+const btn = document.querySelector('.btn-open');
+
+btn.addEventListener('click', function () {
+  console.log('CLICK');
+});
