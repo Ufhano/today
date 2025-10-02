fd9709966dbe1c667161500c39cf003e5735eba3
Ufhano <4035709@myuwc.ac.za>
2025-10-02 23:09:11 +0200

    show form on click

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ console.log('Hello, World!');
 document.querySelector('.btn-open');
 
 const btn = document.querySelector('.btn-open');
+const form = document.querySelector('.fact-form');
 
+//SHARE A fACT BUTTON
 btn.addEventListener('click', function () {
-  console.log('CLICK');
+  if (form.classList.contains('hidden'));
+  {
+    form.classList.remove('hidden');
+  }
 });
