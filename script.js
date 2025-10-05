3364e78b7be99dd4a26988fcf69783f07e34f16e
Ufhano <4035709@myuwc.ac.za>
2025-10-05 21:38:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -121,4 +121,6 @@ const calcFactAge2 = year - 2022;
 //arrow function
 const calcFactAge2 = (year) =>
   //thats a fucntiion btw , now we use a tertnary function
-  year <= new Date().getFullYear();
+  year <= new Date().getFullYear()
+    ? new Date().getFullYear() - year
+    : `impossible `;
