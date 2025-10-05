59901598381904bbee0bf75b700f1d744e34f9c4
Ufhano <4035709@myuwc.ac.za>
2025-10-05 21:08:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -105,5 +105,5 @@ console.log(upperText);
 
 //
 //using these `` means we can poass in a value in the string
-const str = `the current fact is ${text}`;
+const str = `the current fact is "${text}".Its is ${calcFactAge(2015)}`;
 console.log(str);
