084e33d68bfec4e79b35dff782090209f43f6667
Ufhano <4035709@myuwc.ac.za>
2025-10-07 22:06:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -55,15 +55,16 @@ const factsList = document.querySelector('.facts-list');
 factsList.innerHTML = '';
 
 //load data from Supabase
-fetch {"https://vxaviwkqaugxbrgsagdd.supabase.co",{
-  headers:{
-    apikey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4YXZpd2txYXVneGJyZ3NhZ2RkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwOTEzMjQsImV4cCI6MjA3NDY2NzMyNH0.HeiouZfyihPZa69Qqm7xzlu3NVGwFw3YOZntPjfKb3E"
-    ""
-    authorization:
-  }
-}
+const res = fetch('https://vxaviwkqaugxbrgsagdd.supabase.co', {
+  headers: {
+    apikey:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4YXZpd2txYXVneGJyZ3NhZ2RkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwOTEzMjQsImV4cCI6MjA3NDY2NzMyNH0.HeiouZfyihPZa69Qqm7xzlu3NVGwFw3YOZntPjfKb3E',
 
-}
+    authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4YXZpd2txYXVneGJyZ3NhZ2RkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwOTEzMjQsImV4cCI6MjA3NDY2NzMyNH0.HeiouZfyihPZa69Qqm7xzlu3NVGwFw3YOZntPjfKb3E',
+  },
+});
+
 createFactsList(initialFacts);
 
 function createFactsList(dataArray) {
