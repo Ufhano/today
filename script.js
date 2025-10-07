5e67de3e5ba3a5a511b9d1f4c487409a0af375dd
Ufhano <4035709@myuwc.ac.za>
2025-10-07 22:02:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ factsList.innerHTML = '';
 //load data from Supabase
 fetch {"https://vxaviwkqaugxbrgsagdd.supabase.co",{
   headers:{
-    apikey:
+    apikey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4YXZpd2txYXVneGJyZ3NhZ2RkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwOTEzMjQsImV4cCI6MjA3NDY2NzMyNH0.HeiouZfyihPZa69Qqm7xzlu3NVGwFw3YOZntPjfKb3E"
     ""
     authorization:
   }
