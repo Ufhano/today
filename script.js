b87ef905810fda4ca786b3c741c945a19c7cff9d
Ufhano <4035709@myuwc.ac.za>
2025-10-07 05:46:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -34,6 +34,17 @@ const initialFacts = [
 
 document.querySelector('.btn-open'); //selecting the button
 
+const CATEGORIES = [
+  { name: 'technology', color: '#3b82f6' },
+  { name: 'science', color: '#16a34a' },
+  { name: 'finance', color: '#ef4444' },
+  { name: 'society', color: '#eab308' },
+  { name: 'entertainment', color: '#db2777' },
+  { name: 'health', color: '#14b8a6' },
+  { name: 'history', color: '#f97316' },
+  { name: 'news', color: '#8b5cf6' },
+];
+
 //SELECTING DOM ELEMENTS
 const btn = document.querySelector('.btn-open'); //selecting the button
 
