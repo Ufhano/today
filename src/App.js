245da2716072802a85055dc54d8fc8aa620ef07d
Ufhano <4035709@myuwc.ac.za>
2025-10-14 04:26:26 +0200

    importing pic in react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 function App() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
+        <h1>Today I Learned</h1>
+      </div>
+
+      <button class="btn btn-large btn-open">Share a fact</button>
+    </header>
+  );
 }
 
 export default App;
