5b264c5e9f30fb5c5df5f39204e51165cbadff6a
Ufhano Rehoboth Tshivhidzo <101869965+Ufhano@users.noreply.github.com>
2025-10-18 17:36:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -86,7 +86,21 @@ function Header({ showForm, setShowForm }) {
   );
 }
 function NewFactForm() {
-  return <form className="fact-form">Fact form</form>;
+  return (
+    <form className="fact-form">
+      {" "}
+      <input type="text" placeholder="Share a fact with the world..." />
+      <span>200</span>
+      <input type="text" placeholder="Trustworthy source..." />
+      <select>
+        <option value="">Choose category:</option>
+        <option value="technology">Technology</option>
+        <option value="science">Science</option>
+        <option value="finance">Finance</option>
+      </select>
+      <button class="btn btn-large">Post</button>
+    </form>
+  );
 }
 
 const CATEGORIES = [
