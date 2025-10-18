b37bfcffa4a4c9f8621aaae8621d3809d91d2044
Ufhano Rehoboth Tshivhidzo <101869965+Ufhano@users.noreply.github.com>
2025-10-18 12:17:06 +0200

    increment the value

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function Counter() {
   return (
     <div>
       <span style={{ fontSize: "40px" }}>{count}</span>
-      <button className="btn btn-large" onClick={() => setCount(23)}>
+      <button className="btn btn-large" onClick={() => setCount((c) => c + 1)}>
         +1
       </button>
     </div>
