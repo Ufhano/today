a3a934a395f908608ae67314d7e33ab8573a3f5a
Ufhano Rehoboth Tshivhidzo <101869965+Ufhano@users.noreply.github.com>
2025-10-18 11:02:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
 import "./style.css";
 
-const CATEGORIES = [
-  { name: "technology", color: "#3b82f6" },
-  { name: "science", color: "#16a34a" },
-  { name: "finance", color: "#ef4444" },
-  { name: "society", color: "#eab308" },
-  { name: "entertainment", color: "#db2777" },
-  { name: "health", color: "#14b8a6" },
-  { name: "history", color: "#f97316" },
-  { name: "news", color: "#8b5cf6" },
-];
-
 const initialFacts = [
   {
     id: 1,
@@ -75,6 +64,17 @@ function App() {
 function NewFactForm() {
   return <form className="fact-form">Fact form</form>;
 }
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
 //creating new compnents in one file which is not best of practise ,
 function CategoryFilter() {
   return (
