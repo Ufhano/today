1a6f89f78dbf3d9daf26e4a42c5278bec8161621
Ufhano Rehoboth Tshivhidzo <101869965+Ufhano@users.noreply.github.com>
2025-11-10 20:59:35 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://vxaviwkqaugxbrgsagdd.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4YXZpd2txYXVneGJyZ3NhZ2RkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwOTEzMjQsImV4cCI6MjA3NDY2NzMyNH0.HeiouZfyihPZa69Qqm7xzlu3NVGwFw3YOZntPjfKb3E";
 const supabase = createClient(supabaseUrl, supabaseKey);
+// give access to supabase client
+export default supabase;
